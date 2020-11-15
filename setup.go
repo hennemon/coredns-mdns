f409186f8a74d13e3a90381f5bf3537d33c8fa74
hennemon <h3nn3m0n@gmail.com>
2020-11-15 16:27:34 +0100

    fixed wrong caddy import in setup.go

--- setup.go
+++ setup.go
@@ -11,7 +11,7 @@ import (
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
 
-	"github.com/caddyserver/caddy"
+	"github.com/coredns/caddy"
 	"github.com/celebdor/zeroconf"
 )
 
